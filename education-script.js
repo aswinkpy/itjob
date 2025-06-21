6c74bef93e191ab50176e60f54e00f4906be3c06
aswinkpy <aswinkpy2004@gmail.com>
2025-06-21 10:44:10 +0530

    Create education-script.js

---
+++ education-script.js
@@ -0,0 +1,12 @@
+AOS.init({
+  duration: 1000,
+  offset: 120,
+  once: true
+});
+
+// Optional: unlock scrolling after fade-in
+window.onload = () => {
+  setTimeout(() => {
+    document.body.style.overflow = 'auto';
+  }, 1500);
+};
