1b7a30f73e412350f7c872a82991e1a5e083569f
aswinkpy <aswinkpy2004@gmail.com>
2025-06-21 10:16:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+const words = ["WEB DESIGNER", "DEVELOPER", "APP MAKER"];
+const element = document.getElementById("typewriter-text");
+const speed = 100;
+const delayBetweenWords = 2000;
+let wordIndex = 0;
+let charIndex = 0;
+
+function typeWord() {
+  if (charIndex < words[wordIndex].length) {
+    element.textContent += words[wordIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeWord, speed);
+  } else {
+    setTimeout(eraseWord, delayBetweenWords);
+  }
+}
+
+function eraseWord() {
+  if (charIndex > 0) {
+    element.textContent = words[wordIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(eraseWord, speed / 2);
+  } else {
+    wordIndex = (wordIndex + 1) % words.length;
+    setTimeout(typeWord, speed);
+  }
+}
+
+window.onload = () => {
+  typeWord();
+  AOS.init();
+};
